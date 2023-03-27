77b8515b385ecaf0d24fd115fd499b57c4351409
Andrew Wang <andrewxwg@gmail.com>
2023-03-26 21:01:59 -0400

    stuff

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const charsUsed = [];
 var oneFilled;
 var twoFilled;
 var threeFilled;
@@ -18,31 +19,98 @@ function allowDrop(ev) {
   }
 
   function getCharacters(){
-    var d1 = Math.floor(Math.random() * (charList.length - 1));
-    var d2 = Math.floor(Math.random() * (charList.length - 1));
-    var d3 = Math.floor(Math.random() * (charList.length - 1));
+    if (charsUsed.length < charList.length){
+      var d1 = Math.floor(Math.random() * (charList.length - 1));
+      var d2 = Math.floor(Math.random() * (charList.length - 1));
+      var d3 = Math.floor(Math.random() * (charList.length - 1));
+      var monkaW = 0;
+      while (d1 == d2 || d2 == d3 || charsUsed.includes(d2)) {
+        d2 = Math.floor(Math.random() * (charList.length - 1))
+        monkaW += 1;
+        if (monkaW > 67) { //pretty sure the while loop should work fine but just in case OMEGALUL
+            break;
+        }
+      }
+      monkaW = 0;
+      while (d1 == d3 || d2 == d3 || charsUsed.includes(d3)) {
+        d3 = Math.floor(Math.random() * (charList.length - 1))
+        monkaW += 1;
+        console.log("hey");
+        if (monkaW > 67) { //pretty sure the while loop should work fine but just in case OMEGALUL
+            break;
+        }
+      }
+      charsUsed.push(d1);
+      charsUsed.push(d2);
+      charsUsed.push(d3);
+      
+      document.getElementById("drag1").src = charList[d1];
+      document.getElementById("drag2").src = charList[d2];
+      document.getElementById("drag3").src = charList[d3];
+      
+      // const drag = document.createElement("img");
+      // drag.src = charList[d2];
 
-    document.getElementById("drag1").src = charList[d1];
-    document.getElementById("drag2").src = charList[d2];
-    document.getElementById("drag3").src = charList[d3];
-    
-    // const drag = document.createElement("img");
-    // drag.src = charList[d2];
-
-    // document.body.appendChild(drag);
+      // document.body.appendChild(drag);
+    }
   }
 
   function newCharacters(){
+
     var d1 = Math.floor(Math.random() * (charList.length - 1));
     var d2 = Math.floor(Math.random() * (charList.length - 1));
     var d3 = Math.floor(Math.random() * (charList.length - 1));
-
+    var monkaW = 0;
+    while (d1 == d2 || d2 == d3 || charsUsed.includes(d2)) {
+      d2 = Math.floor(Math.random() * (charList.length - 1))
+      monkaW += 1;
+      if (monkaW > 67) { //pretty sure the while loop should work fine but just in case OMEGALUL
+          break;
+      }
+    }
+    monkaW = 0;
+    while (d1 == d3 || d2 == d3 || charsUsed.includes(d3)) {
+      d3 = Math.floor(Math.random() * (charList.length - 1))
+      monkaW += 1;      
+      if (monkaW > 67) { //pretty sure the while loop should work fine but just in case OMEGALUL
+          break;
+      }
+    }
+    charsUsed.push(d1);
+    charsUsed.push(d2);
+    charsUsed.push(d3);
     
-    const ele = document.createElement("img");
-    ele.src = charList[d2];
-    ele.draggable = true;
-    ele.ondragstart = drag()
 
-    document.body.appendChild(ele);
+    const ele1 = document.createElement("img");
+    const ele2 = document.createElement("img");
+    const ele3 = document.createElement("img");
+    defineEle(ele1, d1);
+    defineEle(ele2, d2);
+    defineEle(ele3, d3);
+    
+
+    document.getElementById("chars").appendChild(ele1);
+    document.getElementById("chars").appendChild(ele2);
+    document.getElementById("chars").appendChild(ele3);
+    if (charsUsed.length == 66){
+      document.getElementById("btn").disabled = true;
+    }
+
   }
   
+  function defineEle(ele1, d1){
+    ele1.src = charList[d1];
+    ele1.draggable = true;
+    ele1.ondrag = function(ev){
+      let x = ev.clientX;
+      console.log("X coords: " + x);
+      //      document.getElementById("div1").appendChild(ele);
+      if (x >= 380 && x < 800){
+        document.getElementById("div1").appendChild(ele1);
+      } else if (x >= 820 && x < 1100){
+        document.getElementById("div2").appendChild(ele1);
+      } else if (x >= 1100){
+        document.getElementById("div3").appendChild(ele1);
+      }
+    }
+  }
